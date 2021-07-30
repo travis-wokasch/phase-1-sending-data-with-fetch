6470a1358a0e749516a2eeae055d0e998af08951
Travis <tjwokasch@gmail.com>
2021-07-30 11:23:40 -0600

    Partial complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email){
+    
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            userName: `${name}`, 
+            userEmail:`${email}`
+        })
+    })
+    .then(res => {return res.json()})
+    .then(user => (user))
+
+    .catch(function(error){
+        alert('Invalid user')
+        console.log(error.message)
+    })
+}
+
+submitData()
